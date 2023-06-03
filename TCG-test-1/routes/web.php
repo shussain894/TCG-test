6fbb4417d84d573efdec7faa2e2d8d0e9876d870
Shah Hussain <shahhussain1@me.com>
2023-06-03 21:03:10 +0100

    added some more error handling, on web.php

--- TCG-test-1/routes/web.php
+++ TCG-test-1/routes/web.php
@@ -1,18 +1,41 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\CustomerController;
 use App\Models\Customer;
 
-Route::get('/customer/create', [CustomerController::class, 'create'])->name('customer.create');
-Route::post('/customer', [CustomerController::class, 'store'])->name('customer.store');
-Route::get('/customer', [CustomerController::class, 'index'])->name('customer.index');
+// Each route returns an error response or redirects to an error page
+
+Route::get('/customer/create', function () {
+    try {
+        return (new CustomerController())->create();
+    } catch (\Exception $e) {
+        Log::error('An exception occurred: ' . $e->getMessage());
+    }
+})->name('customer.create');
+
+Route::post('/customer', function (\Illuminate\Http\Request $request) {
+    try {
+        return (new CustomerController())->store($request);
+    } catch (\Exception $e) {
+        Log::error('An error occurred: ' . $e->getMessage());
+    }
+})->name('customer.store');
+
+Route::get('/customer', function (\Illuminate\Http\Request $request) {
+    try {
+        return (new CustomerController())->index($request);
+    } catch (\Exception $e) {
+        Log::error('An error occurred: ' . $e->getMessage());
+    }
+})->name('customer.index');
 
 Route::get('/', function () {
-     // Retrieves all customers from the database
-     $customers = Customer::all();
-
-     // Passes the customers data to the 'welcome' view
-     return view('welcome', ['customers' => $customers]);
+    try {
+        $customers = Customer::all();
+        return view('welcome', ['customers' => $customers]);
+    } catch (\Exception $e) {
+        Log::error('An error occurred: ' . $e->getMessage());
+    }
 })->name('welcome');
-
